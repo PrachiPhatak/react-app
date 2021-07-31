308b25dd2d9d862d99d44c053d7196f013061d64
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-31 14:51:16 -0500

    React hello world

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+
+
+class Counter extends Component {
+    state = {
+        value: this.props.value,
+    };
+
+
+    handleIncrement = () => {
+        this.setState({value: this.state.value + 1});
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-sm bg-secondary">Increment</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const {value} = this.state;
+        return value === 0 ? 'Zero' : value;
+    }
+}
+
+export default Counter;
