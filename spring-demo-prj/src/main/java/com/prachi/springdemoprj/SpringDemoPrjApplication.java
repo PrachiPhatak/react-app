ab9ae9d1c1bcb62a5ff57bb273321166dce428f7
PrachiPhatak <phatak.prachi@gmail.com>
2021-08-31 16:23:12 -0500

    first commit

---
+++ spring-demo-prj/src/main/java/com/prachi/springdemoprj/SpringDemoPrjApplication.java
@@ -0,0 +1,13 @@
+package com.prachi.springdemoprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoPrjApplication.class, args);
+    }
+
+}
